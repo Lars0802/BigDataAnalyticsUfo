2d6fe8c56ae33de5bbdb847dfeb23beb93ee6fdf
Lars Thomsen <lars.thomsen@student.uni-halle.de>
2021-08-18 10:24:36 +0200

    update

--- new.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-import pandas as pd
-
-sighting = pd.read_csv("cit_date.csv")
-station = pd.read_csv("exp_newyorkcity.csv")
-result = open("result.txt", "a")
-
-for i, rows in sighting.iterrows():
-    date = datetime.strptime(station.at[i,'DATE'],'%Y-%m-%dT%H:%M:%S')
--- test.py
+++
@@ -1,8 +0,0 @@
-# Remove duplicate lines
-lines_seen = set()
-outfile = open("inRange_dist.txt", "w")
-for line in open("inRange.txt", "r"):
-    if line not in lines_seen:
-        outfile.write(line)
-        lines_seen.add(line)
-outfile.close()
