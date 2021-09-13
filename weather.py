1a432632707f4e432d27d6919e6c52a2debc7961
Lars Thomsen <lars.thomsen@student.uni-halle.de>
2021-09-13 10:47:48 +0200

    only write if result is number

--- weather.py
+++ weather.py
@@ -5,10 +5,10 @@ from datetime import datetime, timedelta
 from meteostat import Point, Hourly, Daily
 import pandas as pd
 from csv import reader
-import sys
+import os
 
 # open files
-file_path_final = 'sunshine_total.txt'
+file_path_final = 'sunshine_total.csv'
 file_path_lnglat = 'cities_coords.csv'
 file_path_sight = 'sightings.csv'
 
@@ -52,23 +52,25 @@ def write_suntime(city, state, time, sunshine_hourly, sunshine_daily, condition_
 with open(file_path_sight, 'r') as sighting_reader:
     sightings = reader(sighting_reader)
     counter = 1
-    for row in sightings:
+    for row in sightings:      
         try:
+            # get city, state, timedate, lng and lat
             city = get_city().replace(' ', '').lower()
             state = get_state().replace(' ', '').lower()
-
             time_start = get_datetime()
-            time_end = time_start + timedelta(hours=1)
-
+            time_end = time_start + timedelta(seconds=1)
             lat = get_lat(city, state)
             lng = get_lng(city, state)
+
+            # def location
             location = Point(float(lat), float(lng))
 
+            # get results as df
             result_hourly = Hourly(location, time_start, time_end)
             result_daily = Daily(location, time_start, time_end)
 
+            # get hourly data
             data_hourly = result_hourly.fetch()
-
             if data_hourly.empty:
                 sunshine_hourly = 'NaN'
                 condition_code = 'NaN'
@@ -76,15 +78,35 @@ with open(file_path_sight, 'r') as sighting_reader:
                 sunshine_hourly = data_hourly.to_string(columns=['tsun'], index_names=None, header=False, index=False)
                 condition_code = data_hourly.to_string(columns=['coco'], index_names=None, header=False, index=False)
 
+            # get daily data
             data_daily = result_daily.fetch()
             if data_daily.empty:
                 sunshine_daily = 'NaN'
             else:
                 sunshine_daily = data_daily.to_string(columns=['tsun'], index_names=None, header=False, index=False)
 
-            write_suntime(city, state, time_start, sunshine_hourly, sunshine_daily, condition_code)
+            # check for numbers and write to csv
+            if not sunshine_hourly == 'NaN': # true if value is number
+                if not sunshine_daily == 'NaN':
+                    if not condition_code == 'NaN':
+                        df = pd.DataFrame({'city' : [city],
+                                            'state' : [state],
+                                            'time_date' : [time_start],
+                                            'sun_hourly' : [sunshine_hourly],
+                                            'sun_daily' : [sunshine_daily],
+                                            'condition_code' : [condition_code]})
+                        
+                        df.to_csv(file_path_final, mode='a', index=False, header=not os.path.exists(file_path_final))      
+                        #write_suntime(city, state, time_start, sunshine_hourly, sunshine_daily, condition_code)
+
+            # clear df
+            data_hourly = pd.DataFrame()
+            data_daily = pd.DataFrame()
+
             print(counter)
             counter+=1
+            #if counter == 5:
+                #break
         except Exception as e:
             print(e)
             counter+=1
