c814eeef4f6d43e2095b32eb7880d8a9c9943617
Lars Thomsen <lars.thomsen@student.uni-halle.de>
2021-09-13 11:50:38 +0200

    add clear_cache

--- weather.py
+++ weather.py
@@ -34,7 +34,7 @@ def get_datetime(): # returns date and time
             pass
 
 def get_lat(city, state): # returns latitude of given city-state tuple
-    with open(file_path_lnglat, 'r') as lnglat_reader:
+    with open(file_path_lnglat, 'r',) as lnglat_reader:
         coords = reader(lnglat_reader)
         for row in coords:
             if row[0].lower() == city:
@@ -92,22 +92,30 @@ with open(file_path_sight, 'r') as sighting_reader:
                 sunshine_daily = data_daily.to_string(columns=['tsun'], index_names=None, header=False, index=False)
 
             # check for numbers and write to csv
-            if not sunshine_hourly == 'NaN': # true if value is number
-                if not sunshine_daily == 'NaN':
-                    if not condition_code == 'NaN':
-                        df = pd.DataFrame({'city' : [city],
-                                            'state' : [state],
-                                            'time_date' : [time_start],
-                                            'sun_hourly' : [sunshine_hourly],
-                                            'sun_daily' : [sunshine_daily],
-                                            'condition_code' : [condition_code]})
-                        
-                        df.to_csv(file_path_final, mode='a', index=False, header=not os.path.exists(file_path_final))      
-                        #write_suntime(city, state, time_start, sunshine_hourly, sunshine_daily, condition_code)
 
-            # clear df
+            x1 = float(sunshine_hourly)
+            x2 = float(sunshine_daily)
+            x3 = float(condition_code)
+            #print(x3)
+            #print(pd.isna(x3))
+
+            if not pd.isna(x1) or not pd.isna(x2) or not pd.isna(x3): # true if value is number
+                df = pd.DataFrame({'city' : [city],
+                                    'state' : [state],
+                                    'time_date' : [time_start],
+                                    'sun_hourly' : [sunshine_hourly],
+                                    'sun_daily' : [sunshine_daily],
+                                    'condition_code' : [condition_code]})
+                
+                df.to_csv(file_path_final, mode='a', index=False, header=not os.path.exists(file_path_final))      
+                #write_suntime(city, state, time_start, sunshine_hourly, sunshine_daily, condition_code)
+
+            # clear df and cache
             data_hourly = pd.DataFrame()
             data_daily = pd.DataFrame()
+            df = pd.DataFrame()
+            Daily.clear_cache()
+            Hourly.clear_cache()
 
             print(counter)
             counter+=1
