af15f64a338457502dc54904d2705799134b357d
Lars Thomsen <lars.thomsen@student.uni-halle.de>
2021-09-14 17:09:23 +0200

    save figs

--- map.py
+++ map.py
@@ -11,28 +11,41 @@ df.sort_values(by='time_date', ascending=False, inplace=True)
 
 #%%
 # hourly_suntime
-plt.figure(figsize=(10,5))
+plt.figure(figsize=(16,9))
 plt.plot(df.time_date, df.sun_hourly, 'r.-')
 plt.title('Sonnenminuten pro Stunde je Sichtung')
 plt.ylabel('Sonnenminuten/Stunde')
 plt.xlabel('Datum')
 plt.show()
+try:
+    plt.savefig('img/hourly_suntime.png', dpi=(1920/16))
+except Exception as e:
+    print(e)
 
 #%%
 # daily_suntime
-plt.figure(figsize=(10,5))
+plt.figure(figsize=(16,9))
 plt.plot(df.time_date, df.sun_daily, 'b.-')
 plt.title('Sonnenminuten pro Tag je Sichtung')
 plt.ylabel('Sonnenminuten/Tag')
 plt.xlabel('Datum')
 plt.show()
+try:
+    plt.savefig('img/daily_suntime.png', dpi=(1920/16))
+except Exception as e:
+    print(e)
 
 #%%
 # condition_codes
-plt.figure(figsize=(10,5))
+plt.figure(figsize=(16,9))
 plt.plot(df.time_date, df.condition_code, 'g.-')
 plt.title('Himmelkonditionen je Sichtung')
 plt.ylabel('Conditioncode')
 plt.xlabel('Datum')
 plt.show()
+try:
+    plt.savefig('img/condition_codes.png', dpi=(1920/16))
+except Exception as e:
+    print(e)
+
 #%%
