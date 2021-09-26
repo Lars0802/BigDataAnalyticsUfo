32c57c432006e2de354df6b3a90814a1b69b1148
Lars Thomsen <lars.thomsen@student.uni-halle.de>
2021-09-27 00:29:03 +0200

    clean up

--- database_requests.py
+++ database_requests.py
@@ -1,28 +1,42 @@
-#Lars Thomsen, anhqn
-#02.09.2021
-
 import mysql.connector
 import csv
 import sys
 
 #connect to database
 db = mysql.connector.connect(host="85.13.154.57", user="d03664f3", passwd="BigDataUfoPW", db="d03664f3")
-res = db.cursor()
-statement = "select city, state, date__time from sighting where not (city is null or city = '' or city = '?' or date__time is null or date__time = '' or date__time = '?')"
+res_1 = db.cursor()
+res_2 = db.cursor()
+statement_1 = "select city, state, date__time from sighting where not (city is null or city = '' or city = '?' or date__time is null or date__time = '' or date__time = '?')"
+statement_2 = "select distinct city, state from sighting where not (city is null or city = '' or city = '?')"
 file_path = 'data/sightings.csv'
+file_cit = 'data/cit.csv'
 
 #abfrage
 try:
-    res.execute(statement)
-    rows = res.fetchall()
+    res_1.execute(statement_1)
+    rows_1 = res_1.fetchall()
+    res_1.close()
+
+    res_2.execute(statement_2)
+    rows_2 = res_2.fetchall()
+    res_2.close()
 finally:
     db.close()
 
 #in csv speichern
-if rows:
+if rows_1:
     fs = open(file_path, 'w', newline = '')
     ergebnisse = csv.writer(fs)
-    ergebnisse.writerows(rows)
+    ergebnisse.writerows(rows_1)
+    fs.close()
+else:
+    sys.exit("Keine Ergebnisse")
+
+#in txt speichern
+if rows_2:
+    fs = open(file_cit, 'w', newline = '')
+    ergebnisse = csv.writer(fs)
+    ergebnisse.writerows(rows_2)
     fs.close()
 else:
     sys.exit("Keine Ergebnisse")
--- geocoding.py
+++ geocoding.py
@@ -3,30 +3,31 @@ import requests
 import json
 import geopy.distance
 #####################################################################################################
-data = pd.read_csv("stations.csv")
 
-file_cities = "data/cit.txt"
-
+file_cities = "data/cit.csv"
+count = 1
 
 cities = open(file_cities, "r") #input cities from db
-cities_coords = open("data/cities_coords.txt", "a") #output cites + coords
+cities_coords = open("data/cities_coords.csv", "a") #output cites + coords
+cities_coords.write('city, state, lng, lat\n')
 next = cities.readline()
+
 while next:
-    parameter = {"key" : "EVL3umyRakXGnrfaVMmrL2evEScjo4uG", "location" : next}
+    if count < 14950: #geocoding api erlaubt nur 15000 requests pro monat
+        parameter = {"key" : "xkmKXYk7RidnvbHUHxAbHisL29iUDmXF", "location" : next}
+    else:
+        parameter = {"key" : "EVL3umyRakXGnrfaVMmrL2evEScjo4uG", "location" : next}
+
     response = requests.get("http://www.mapquestapi.com/geocoding/v1/address", params = parameter)
     coords = json.loads(response.text)['results']
+    count += 1
 
+    # save lng lat
     lat = coords[0]['locations'][0]['latLng']['lat']
     lng = coords[0]['locations'][0]['latLng']['lng']
     print(next, lat, lng)
     cities_coords.write(next.rstrip('\n') + ', ' + str(lat) + ', ' + str(lng) + '\n')
-
-    for i, row in data.iterrows():
-        lat_station = data.at[i,'latitude']
-        lng_station = data.at[i,'longitude']
-        if geopy.distance.distance((lat,lng),(lat_station,lng_station)) <= 20:
-            print ('Sighting ' + next.rstrip('\n') + ' in range of ' + data.at[i,'name'])
-
     next = cities.readline()
+
 cities.close()
 cities_coords.close()
